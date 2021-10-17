7cace89a8f73baeb61f50f5da20f722e17e80f80
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-10-18 06:29:14 +0700

    rombak axios

--- src/javascript/other.js
+++ src/javascript/other.js
@@ -2,14 +2,19 @@ import X from "./index.js";
 import Axios from "./utils/AxiosBeLike.js";
 
 X.a();
-Axios.Post(
-    "index.php",
-    { test: 123, a: "aaa", b: 10.02, c: ["1", "2", "3"], d: [1, 2, 3] },
-    function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("result").innerHTML = this.responseText;
-        } else if (this.status >= 400) {
-            document.getElementById("result").innerHTML = this.statusText;
-        }
-    }
-);
+// Axios.Post(
+//     "index.php",
+//     { test: 123, a: "aaa", b: 10.02, c: ["1", "2", "3"], d: [1, 2, 3] },
+//     function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             document.getElementById("result").innerHTML = this.responseText;
+//         } else if (this.status >= 400) {
+//             document.getElementById("result").innerHTML = this.statusText;
+//         }
+//     }
+// );
+
+const params = {
+    "q": "str"
+}
+Axios.Get("index.php", params).then((a) => console.log(a))
--- src/javascript/utils/AxiosBeLike.js
+++ src/javascript/utils/AxiosBeLike.js
@@ -2,25 +2,32 @@
  *
  * @param {string} url
  * @param {object} body
- * @param {function} onReadyFunction
  */
-const postFunction = (url, body, onReadyFunction) => {
+
+const postFunction = (url, body, option = {}) => new Promise((resolve, reject) => {
     const xmlHttp = new XMLHttpRequest();
     xmlHttp.open("POST", "php/" + url);
     xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlHttp.onreadystatechange = onReadyFunction;
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
+            resolve(xmlHttp.responseText)
+            return
+        } else if (this.readyState === XMLHttpRequest.DONE) {
+            reject(xmlHttp.responseText)
+        }
+    }
     const bodyData = new URLSearchParams();
     bodyData.set("data", JSON.stringify(body));
     xmlHttp.send(bodyData.toString());
-};
+})
 
 /**
  *
  * @param {string} url
  * @param {object} params
- * @param {function} onReadyFunction
  */
-const getFunction = (url, params, onReadyFunction) => {
+
+const getFunction = (url, params) => new Promise((resolve, reject) => {
     const xmlHttp = new XMLHttpRequest();
     const parameterData = new URLSearchParams();
     Object.keys(params).forEach((key) => {
@@ -28,9 +35,16 @@ const getFunction = (url, params, onReadyFunction) => {
     });
     const parameter = parameterData.toString();
     xmlHttp.open("GET", "php/" + url + (parameter.length > 0 ? "?" + parameter : ""));
-    xmlHttp.onreadystatechange = onReadyFunction;
+    xmlHttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status == 200) {
+            resolve(xmlHttp.responseText)
+        } else if (this.readyState === XMLHttpRequest.DONE) {
+            reject(xmlHttp.responseText)
+        }
+    }
     xmlHttp.send();
-};
+})
+
 
 export default {
     Post: postFunction,
