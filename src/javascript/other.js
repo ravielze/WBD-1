d0c8d208e2880cb162fe4d9c952524a723e60705
ravielze <ravielz20@gmail.com>
2021-10-17 18:03:00 +0700

    Update other.js for example

--- src/javascript/other.js
+++ src/javascript/other.js
@@ -2,14 +2,11 @@ import X from "./index.js";
 import Axios from "./utils/AxiosBeLike.js";
 
 X.a();
-Axios.Post(
-    "index.php",
-    { test: 123, a: "aaa", b: 10.02, c: ["1", "2", "3"], d: [1, 2, 3] },
-    function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("result").innerHTML = this.responseText;
-        } else if (this.status >= 400) {
-            document.getElementById("result").innerHTML = this.statusText;
-        }
+Axios.Post("login.php", { username: "abc" }, function () {
+    if (this.readyState == 4 && this.status == 200) {
+        const obj = JSON.parse(this.responseText);
+        console.log(obj);
+    } else if (this.status >= 400) {
+        document.getElementById("result").innerHTML = this.statusText;
     }
-);
+});
