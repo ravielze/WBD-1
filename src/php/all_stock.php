ed57e89b3a445f96008e5fdeb598dec84f7bcf9d
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-10-19 10:46:40 +0700

    get all stock

---
+++ src/php/all_stock.php
@@ -0,0 +1,13 @@
+<?php
+include "utils/method_checker.php";
+include "utils/key_checker.php";
+include "database/connection.php";
+AllowedMethod("GET");
+
+$c = ConnectDatabase();
+$q = $c->query("SELECT * FROM dorayakis");
+$result = $q->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($result);
+
+?>
