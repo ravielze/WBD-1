07d13d982017b084d2d7934d91b572f38aa6df86
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-10-22 00:13:46 +0700

    Added db rendering for dashboard (not yet tested)

--- src/javascript/dashboard.js
+++ src/javascript/dashboard.js
@@ -1,10 +1,25 @@
 import { renderProducts } from './utils/RenderProducts.js';
+import Axios from './utils/AxiosBeLike.js';
 
 // Dummy Dorayakis
 // Later will be implemented by fetching data
 // from sqlite
+/**
+ * TEMPORARY RENDERER
+ */
 import tempDorayakis from './shared/Dorayakis.js';
 
-for (let i = 0; i < 2; i++) {
-  renderProducts(tempDorayakis);
+renderProducts(tempDorayakis);
+
+/**
+ * MAIN RENDERER
+ */
+function getAllDorayakis() {
+  return Axios.Get('products/dashboard.php').then((res) => JSON.parse(res));
 }
+
+getAllDorayakis()
+  .then((dorayakis) => {
+    renderProducts(dorayakis);
+  })
+  .catch((err) => console.log(err));
--- src/javascript/utils/RenderProducts.js
+++ src/javascript/utils/RenderProducts.js
@@ -11,19 +11,22 @@ export function renderProducts(array) {
     const productCard = document.createElement('figure');
     productCard.className = 'product-card';
 
-    const img = createImg(item.picture, item.name);
+    const img = createImg(item.picture, item.name); // CHANGE THIS TO item.dorayaki FOR REAL RENDERING
 
     const cardText = document.createElement('figcaption');
     cardText.className = 'card-text';
     cardText.innerHTML = `\
-    <h3>${item.name}</h3>\
+    <h3>${item.name}</h3>\ 
     <p>${item.description}</p>\
     <p>Stok tersedia: ${item.stock}</p>
-    `;
+    `; // CHANGE THIS TO item.dorayaki, item.descript, item.amount FOR REAL RENDERING
 
     // Append from child to parents
     productCard.append(img);
     productCard.append(cardText);
+    productCard.onclick = () => {
+      location.href = `./product.html?pid=${item.id_dorayaki}`; // CHANGE THIS TO item.dorayaki FOR REAL RENDERING
+    };
 
     cardContainer.append(productCard);
 
